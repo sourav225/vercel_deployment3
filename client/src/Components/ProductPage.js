7c6bf3c789bce509f88283581b73d542a9c52df5
sourav225 <68838824+sourav225@users.noreply.github.com>
2024-04-16 15:07:07 +0530

    Update ProductPage.js

--- client/src/Components/ProductPage.js
+++ client/src/Components/ProductPage.js
@@ -24,7 +24,7 @@ function ProductPage() {
   const handleClick = async (idi) => {
     const isuserEmail=document.cookie?.split(";").find((cookie)=>cookie.trim().startsWith("useremail="))?.split("=")[1];
     axios
-      .get(`https://vercel-deployment3-server3.vercel.app/api/addtocart/${idi}`,{useremail:isuserEmail})
+      .post(`https://vercel-deployment3-server3.vercel.app/api/addtocart/${idi}`,{useremail:isuserEmail})
       .then((response) => {
         setstatus(response.data.res || {}); // Assuming the data structure is an array
       })
