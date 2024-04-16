7ef47335747d158d14b8430c44444eb81ce331e9
sourav225 <68838824+sourav225@users.noreply.github.com>
2024-04-16 15:04:40 +0530

    Update Cart.js

--- client/src/Components/Cart.js
+++ client/src/Components/Cart.js
@@ -32,7 +32,7 @@ function Cart() {
   const handleClick = async (idi) => {
     const isuserEmail=document.cookie?.split(";").find((cookie)=>cookie.trim().startsWith("useremail="))?.split("=")[1];
     axios
-      .get(`https://vercel-deployment3-server3.vercel.app/api/addtocart/${idi}`, { useremail:isuserEmail })
+      .post(`https://vercel-deployment3-server3.vercel.app/api/addtocart/${idi}`, { useremail:isuserEmail })
       .then((response) => {
         setstatus(response.data.res || {}); // Assuming the data structure is an array
       })
@@ -41,7 +41,7 @@ function Cart() {
   const handleMinusClick = async (idi) => {
     const isuserEmail=document.cookie.split(";")?.find((cookie)=>cookie.trim().startsWith("useremail="))?.split("=")[1];
     axios
-      .get(`https://vercel-deployment3-server3.vercel.app/api/minustocart/${idi}`, { useremail:isuserEmail })
+      .post(`https://vercel-deployment3-server3.vercel.app/api/minustocart/${idi}`, { useremail:isuserEmail })
       .then((response) => {
         setstatus(response.data.res || {}); // Assuming the data structure is an array
       })
@@ -50,7 +50,7 @@ function Cart() {
   const handleRemove = async (idi) => {
     const isuserEmail=document.cookie?.split(";").find((cookie)=>cookie.trim().startsWith("useremail="))?.split("=")[1];
     axios
-      .get(`https://vercel-deployment3-server3.vercel.app/api/removefromcart/${idi}`, { useremail: isuserEmail })
+      .post(`https://vercel-deployment3-server3.vercel.app/api/removefromcart/${idi}`, { useremail: isuserEmail })
       .then((response) => {
         setstatus(response.data.res || {}); // Assuming the data structure is an array
       })
